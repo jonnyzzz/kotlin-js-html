0178ba11b23f95628046ff40b2536be9f33d7505
Eugene Petrenko <me@jonnyzzz.com>
2021-09-22 15:17:10 +0200

    tune lambda - now it is able to extract post-data and compute sha256

--- aws-reception-lambda/main.go
+++ aws-reception-lambda/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	runtime "github.com/aws/aws-lambda-go/lambda"
@@ -15,8 +17,32 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 		fmt.Printf("    %s: %s\n", key, value)
 	}
 
-	return events.APIGatewayProxyResponse{Body: request.Body + " this is our lambda", StatusCode: 200}, nil
+	var kotlinCode []byte
+	if request.IsBase64Encoded {
+		data, err := base64.StdEncoding.DecodeString(request.Body)
+		if err != nil {
+			fmt.Printf("Failed to decode body %s", err.Error())
+			return events.APIGatewayProxyResponse{Body: "Failed to parse script", StatusCode: 400}, nil
+		}
+		kotlinCode = data
+	} else {
+		kotlinCode = []byte(request.Body)
+	}
+
+	sha := sha256.Sum256(kotlinCode)
+	shaText := base64.URLEncoding.EncodeToString(sha[:])
+	fmt.Printf("Kotlin Code hash = %s\n", shaText)
+
+	return events.APIGatewayProxyResponse{
+		Body: fmt.Sprintf("this is our lambda:%s\n%s\n", shaText, string(kotlinCode)),
+		Headers: map[string]string{
+			"ETag":         shaText,
+			"Content-Type": "application/json",
+		},
+		StatusCode: 200,
+	}, nil
 }
+
 func main() {
 	runtime.Start(handleRequest)
 }
--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -4,7 +4,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e -x -u
 
 echo "Building lambda..."
-../aws-reception-lamnda/build.sh
+./../aws-reception-lambda/build.sh
 
 
 terraform apply -auto-approve
