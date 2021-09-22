31527bb731783fa91168343ca9cf7e32ac36557b
Eugene Petrenko <me@jonnyzzz.com>
2021-09-22 21:57:40 +0200

    missing script

---
+++ aws-builder-lambda/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e -x -u
+
+rm -rf build || true
+mkdir build || true
+
+go version
+
+NAME=lambda-builder
+GOOS=linux GOARCH=amd64 go build -o "build/$NAME" *.go
+
+### this file is created via Terraform, run terraform apply if you miss it
+. ./generated_variables.sh
+
+docker build -t $NAME .
+
+## you need AWS CLI to run this
+aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${ECR_URL}"
+
+docker tag  $NAME:latest "${ECR_URL}:${ECR_TAG}"
+docker push "${ECR_URL}:${ECR_TAG}"
