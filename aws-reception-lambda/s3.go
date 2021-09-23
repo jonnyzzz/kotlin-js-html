f33f0ef6b26dd69889ed0415a1d6ba216ef1d31a
Eugene Petrenko <me@jonnyzzz.com>
2021-09-23 03:56:52 +0200

    cleanup

--- aws-reception-lambda/s3.go
+++ aws-reception-lambda/s3.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"log"
 )
 
 var s3Service *s3.S3
@@ -19,26 +16,4 @@ func init() {
 
 	s3Service = s3.New(sess)
 	ecsClient = ecs.New(sess)
-
-	TestS3()
-}
-
-func TestS3() {
-	bucketName := GetCacheBucketName()
-	log.Println("Processing bucket: ", bucketName)
-
-	///this is warmup test for the lambda
-	{
-		objects, err := s3Service.ListObjects(&s3.ListObjectsInput{
-			Bucket: aws.String(bucketName),
-		})
-
-		if err != nil {
-			log.Panic("Failed to read bucket ", err.Error(), err)
-		}
-
-		for _, v := range objects.Contents {
-			fmt.Println("Object: ", *v.Key)
-		}
-	}
 }
