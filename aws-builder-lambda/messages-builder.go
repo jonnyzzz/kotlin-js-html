55b725112eefda9115177cdd7a30eb2ec20c111b
Eugene Petrenko <me@jonnyzzz.com>
2021-09-23 16:09:54 +0200

    normalize/reuse messages

--- aws-builder-lambda/messages-builder.go
+++ aws-builder-lambda/messages-builder.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"log"
 )
-
-type EcsDoneResultFile struct {
-	BucketKey string `json:"key"`
-	CdnUrl    string `json:"cnd_url"`
-}
-
-type EcsDoneResult struct {
-	Type   string              `json:"type"`
-	Status string              `json:"status"`
-	Files  []EcsDoneResultFile `json:"files"`
-}
 
 func PublishS3PendingStatus(shaText string, status string) {
 	PublishS3PendingStatusWithFiles(shaText, status, []EcsDoneResultFile{})
@@ -26,19 +14,15 @@ func PublishS3PendingStatus(shaText string, status string) {
 func PublishS3PendingStatusWithFiles(shaText string, status string, files []EcsDoneResultFile) {
 	cacheBucketResultKey := GetCacheBucketResponsePath(shaText)
 
-	payload, err := json.MarshalIndent(EcsDoneResult{
-		Type:   "builder-task",
-		Status: status,
-		Files:  files,
-	}, "", "  ")
+	payload := finalPayload(shaText, status, files, "Not Implemented")
 
-	if err != nil {
-		log.Panic("Failed to serialize retry message", err.Error(), err)
-	}
-
-	_, err = s3Service.PutObject(&s3.PutObjectInput{
+	_, err := s3Service.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(GetCacheBucketName()),
 		Key:    aws.String(cacheBucketResultKey),
 		Body:   bytes.NewReader(payload),
 	})
+
+	if err != nil {
+		log.Panicf("Failed to write results to S3 to %s: %v\n", cacheBucketResultKey, err)
+	}
 }
--- aws-reception-lambda/messages-reception.go
+++ aws-reception-lambda/messages-reception.go
@@ -5,11 +5,18 @@ import (
 	"log"
 )
 
+type EcsDoneResultFile struct {
+	BucketKey string `json:"key"`
+	CdnUrl    string `json:"cnd_url"`
+}
+
 type RetryAfterTimeoutMessage struct {
-	Type          string `json:"type"`
-	TimeoutMillis int    `json:"timeout_millis"`
-	Sha256        string `json:"sha_256"`
-	Reason        string `json:"reason"`
+	Type          string              `json:"type"`
+	TimeoutMillis int                 `json:"timeout_millis"`
+	Sha256        string              `json:"sha_256"`
+	Reason        string              `json:"reason"`
+	Files         []EcsDoneResultFile `json:"files"`
+	Output        string              `json:"log_output"`
 }
 
 func temporaryPayload(shaText string, reason string) []byte {
@@ -18,6 +25,22 @@ func temporaryPayload(shaText string, reason string) []byte {
 		TimeoutMillis: 777,
 		Sha256:        shaText,
 		Reason:        reason,
+	}, "", "  ")
+
+	if err != nil {
+		log.Panic("Failed to serialize retry message", err.Error(), err)
+	}
+	return data
+}
+
+func finalPayload(shaText string, reason string, files []EcsDoneResultFile, logOutput string) []byte {
+	data, err := json.MarshalIndent(RetryAfterTimeoutMessage{
+		Type:          "final",
+		TimeoutMillis: 0,
+		Sha256:        shaText,
+		Reason:        reason,
+		Files:         files,
+		Output:        logOutput,
 	}, "", "  ")
 
 	if err != nil {
