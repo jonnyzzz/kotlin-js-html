b013ee90c3529d708d7b0bf4e11cf09f75b2ed16
Eugene Petrenko <me@jonnyzzz.com>
2021-09-23 02:23:47 +0200

    simplify fargate runner

--- aws-builder-lambda/main.go
+++ aws-builder-lambda/main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+func main() {
 	fmt.Println("Running Gradle build...")
 	RunGradle()
-	return fmt.Sprintf("Hello %s!", name.Name), nil
-}
-
-func main() {
-	lambda.Start(HandleRequest)
 }
