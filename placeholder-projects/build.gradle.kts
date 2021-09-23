012e066e07af6ef5495800ee5cd763461d81a315
Maciej Procyk <maciej.procyk@jetbrains.com>
2021-09-23 21:12:03 +0200

    Add dependencies object, only mark as dependsOn

--- placeholder-projects/build.gradle.kts
+++ placeholder-projects/build.gradle.kts
@@ -9,9 +9,10 @@ subprojects {
   version = "1.0"
 }
 
+project.subprojects.map { ":${it.name}:fullDistBuildInner" }.zipWithNext()
+  .forEach { (fst, snd) -> project.tasks.getByPath(fst).mustRunAfter(project.tasks.getByPath(snd)) }
+
 val fullDistBuild by tasks.creating {
-
-
   project.subprojects.map {
     dependsOn(":${it.path}:fullDistBuildInner")
   }
