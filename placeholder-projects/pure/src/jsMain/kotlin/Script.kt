f3dbb1541e6d8eff6d5e6861aac7f6fff810de7e
avan1235 <macieekprocyk@gmail.com>
2021-10-06 19:34:03 +0200

    Clean placeholders `Script` files

--- placeholder-projects/pure/src/jsMain/kotlin/Script.kt
+++ placeholder-projects/pure/src/jsMain/kotlin/Script.kt
@@ -1,15 +1,1 @@
-import kotlinx.browser.*
-import kotlinx.html.*
-import kotlinx.html.dom.*
-
-
-
-@JsModule("is-sorted")
-@JsNonModule
-external fun <T> sorted(a: Array<T>): Boolean
-
-fun main() {
-  window.alert("Hello Kotlin/JS in HTML world")
-  val data = arrayOf(3,1,2)
-  window.alert("$data is ${if (sorted(data)) "" else "not "}sorted")
-}
+fun main() = Unit
